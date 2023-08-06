4f76b33f78f608d61234711f6554dd74a644245c
Faruk Erdem <farukomererdem2003@gmail.com>
2023-08-06 14:33:11 +0300

    Argument options

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import time
 import speech_recognition as sr
+import argparse
 
-def recognize_speech_from_mic(recognizer,microphone):
+def get_user_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m","--microphone",dest="mic",help="Index of microphone device. For microphone device list: --microphones",type=str)
+    parser.add_argument("--microphones",required=False,default=False,action="store_true",help="List microphones.")
+    parser.add_argument("-l","--language",dest="lang",default="tr-TR",type=str,help="Language tag. For language list: https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages")
+    args = parser.parse_args()
+    return args
+    
+
+def recognize_speech_from_mic(recognizer,microphone,language):
     if not isinstance(recognizer,sr.Recognizer):
         raise TypeError("`recognizer` must be `Recognizer` instance.")
     if not isinstance(microphone,sr.Microphone):
@@ -19,7 +29,7 @@ def recognize_speech_from_mic(recognizer,microphone):
         }
 
     try:
-        response["transcription"] = recognizer.recognize_google(audio,language="tr-TR")
+        response["transcription"] = recognizer.recognize_google(audio,language=language)
     except  sr.RequestError:
         response["success"] = False
         response["error"] = "API unavaliable."
@@ -30,14 +40,25 @@ def recognize_speech_from_mic(recognizer,microphone):
 
 
 if __name__ == "__main__":
+    args = get_user_input()
+    if args.microphones:
+        mics = sr.Microphone.list_microphone_names()
+        n=0
+        for i in mics:
+            print(f"[{n}] {str(i)}")
+        quit()
+
+    language = args.lang
+    microphone_index = args.mic
+
     word = "öyle"
     word = word.lower()
     print("Listening...")
     recognizer = sr.Recognizer()
-    microphone = sr.Microphone(device_index=1)
-
+    microphone = sr.Microphone(device_index=microphone_index)
+    
     while True:
-        speech = recognize_speech_from_mic(recognizer,microphone)
+        speech = recognize_speech_from_mic(recognizer,microphone,language)
         words = str(speech["transcription"]).split(" ")  
         print(words)
         for i in words:
